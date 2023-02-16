09743073924a925030c731aff1ec113702d38dcd
Latheesh9865 <113575192+Latheesh9865@users.noreply.github.com>
2023-02-16 16:04:26 +0530

    Add files via upload

---
+++ patern5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int i,j,n,m;
+	cin>>n>>m;
+	for(j=1;j<=n;j++)
+	{
+		for(i=1;i<=m;i++)
+		{
+			cout<<"*";
+		}
+		cout<<"\n";
+		
+	}
+	return 0;
+}
